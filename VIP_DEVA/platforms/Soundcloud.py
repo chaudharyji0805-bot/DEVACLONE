a076822c5d7b75d3f00f6307111b38f78fd83553
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 13:23:06 +0530

    Update Soundcloud.py

--- VIP_DEVA/platforms/Soundcloud.py
+++ VIP_DEVA/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from VIP_INNOCENT.utils.formatters import seconds_to_min
+from VIP_DEVA.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
