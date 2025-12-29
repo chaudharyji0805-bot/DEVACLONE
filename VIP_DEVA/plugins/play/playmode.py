84ab2d99822c2f6a5fd44745add1e6a29e396232
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 13:55:47 +0530

    Update playmode.py

--- VIP_DEVA/plugins/play/playmode.py
+++ VIP_DEVA/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from VIP_INNOCENT import app
-from VIP_INNOCENT.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from VIP_INNOCENT.utils.decorators import language
-from VIP_INNOCENT.utils.inline.settings import playmode_users_markup
+from VIP_DEVA import app
+from VIP_DEVA.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from VIP_DEVA.utils.decorators import language
+from VIP_DEVA.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
