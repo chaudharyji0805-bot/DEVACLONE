2834543b205eb0dca8658615b3b30aace029b8d9
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 13:50:55 +0530

    Update info.py

--- VIP_DEVA/plugins/misc/info.py
+++ VIP_DEVA/plugins/misc/info.py
@@ -2,7 +2,7 @@ import asyncio, os, time, aiohttp
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
-from VIP_INNOCENT import app
+from VIP_DEVA import app
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
@@ -66,8 +66,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "VIP_INNOCENT/assets/userinfo.png"
-font_path = "VIP_INNOCENT/assets/hiroko.ttf"
+bg_path = "VIP_DEVA/assets/userinfo.png"
+font_path = "VIP_DEVA/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
