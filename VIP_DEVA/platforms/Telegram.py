6b4eeb591fee2aabb44c8b395bd17c0386733188
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 13:23:35 +0530

    Update Telegram.py

--- VIP_DEVA/platforms/Telegram.py
+++ VIP_DEVA/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from VIP_INNOCENT import app
-from VIP_INNOCENT.utils.formatters import (
+from VIP_DEVA import app
+from VIP_DEVA.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
