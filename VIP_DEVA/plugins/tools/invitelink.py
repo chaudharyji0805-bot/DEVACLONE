889da7a16cc6f57b6f762a3ae90b298ff83cfb62
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 14:02:19 +0530

    Update invitelink.py

--- VIP_DEVA/plugins/tools/invitelink.py
+++ VIP_DEVA/plugins/tools/invitelink.py
@@ -1,11 +1,11 @@
-from VIP_INNOCENT import app
+from VIP_DEVA import app
 from pyrogram import Client, filters
 from pyrogram.errors import ChatIdInvalid
 from pyrogram.errors import ChatAdminRequired, ChatNotModified, ChatIdInvalid, FloodWait, InviteHashExpired, UserNotParticipant
 import os
 import json
 from pyrogram.types import Message
-from VIP_INNOCENT.misc import SUDOERS
+from VIP_DEVA.misc import SUDOERS
 
 
 
