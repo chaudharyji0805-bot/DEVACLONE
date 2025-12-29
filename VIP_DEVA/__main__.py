a8d9b1230242dec0068af6df71c467d79152e200
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-30 00:14:10 +0530

    Update __main__.py

--- VIP_DEVA/__main__.py
+++ VIP_DEVA/__main__.py
@@ -6,13 +6,13 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from VIP_INNOCENT import LOGGER, app, userbot
-from VIP_INNOCENT.core.call import INNOCENT
-from VIP_INNOCENT.misc import sudo
-from VIP_INNOCENT.plugins import ALL_MODULES
-from VIP_INNOCENT.utils.database import get_banned_users, get_gbanned
+from VIP_DEVA import LOGGER, app, userbot
+from VIP_DEVA.core.call import DEVA
+from VIP_DEVA.misc import sudo
+from VIP_DEVA.plugins import ALL_MODULES
+from VIP_DEVA.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-from VIP_INNOCENT.plugins.tools.clone import restart_bots
+from VIP_DEVA.plugins.tools.clone import restart_bots
 
 
 async def init():
@@ -31,28 +31,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("VIP_INNOCENT.plugins" + all_module)
-    LOGGER("VIP_INNOCENT.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        importlib.import_module("VIP_DEVA.plugins" + all_module)
+    LOGGER("VIP_DEVA.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
     await userbot.start()
-    await INNOCENT.start()
+    await DEVA.start()
     try:
-        await INNOCENT.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await DEVA.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("VIP_INNOCENT").error(
+        LOGGER("VIP_DEVA").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
         exit()
     except:
         pass
-    await INNOCENT.decorators()
+    await DEVA.decorators()
     await restart_bots()
-    LOGGER("VIP_INNOCENT").info(
+    LOGGER("VIP_DEVA").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 VERON 𝗕𝗼t𝘀☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("VIP_INNOCENT").info("𝗦𝗧𝗢𝗣 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+    LOGGER("VIP_DEVA").info("𝗦𝗧𝗢𝗣 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
