2cf38706fea984f0849a703c615e61453bbe2bd2
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 20:17:36 +0530

    Update extraction.py

--- VIP_DEVA/utils/extraction.py
+++ VIP_DEVA/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from VIP_INNOCENT import app
+from VIP_DEVA import app
 
 
 async def extract_user(m: Message) -> User:
