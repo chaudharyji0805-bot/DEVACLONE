bef27f810645598306f713100d19ebe936121b52
BOTZEMPIRE <chaudharyji0805@gmail.com>
2025-12-29 13:54:22 +0530

    Update watcher.py

--- VIP_DEVA/plugins/misc/watcher.py
+++ VIP_DEVA/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIP_INNOCENT import app
-from VIP_INNOCENT.core.call import INNOCENT
+from VIP_DEVA import app
+from VIP_DEVA.core.call import DEVA
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await INNOCENT.stop_stream_force(message.chat.id)
+    await DEVA.stop_stream_force(message.chat.id)
